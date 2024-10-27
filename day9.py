21a514d007648e6cb11c740dbb59ee7352a292c2
Habibou Halirou <habibhalirou@gmail.com>
2024-10-27 16:25:31 +0100

    jour Neuf

---
+++ day9.py
@@ -0,0 +1,30 @@
+import re
+from os.path import split
+
+with open("entrer", "r") as file :
+    historiques = file.readlines()
+
+#fonction qui retourne un tableau contenant la difference entre deux élements consécutifs du tableau fourni en paramètre
+def difference(historique):
+    etape_difference = []
+    for i in range(len(historique)-1):
+        difference = int(historique[i+1]) - int(historique[i])
+        etape_difference.append(difference)
+    return etape_difference
+
+somme = 0
+for historique in historiques :
+    historique = re.split(r" ", historique.strip())
+    etape = difference(historique)
+    #initialisation du tableau histoires
+    histoires = [int(historique[-1]),etape[-1]]
+    #print(historique)
+    #print(etape)
+
+    #je teste si la liste ne contient pas que des 0
+    while not all(x==0 for x in etape):
+        etape = difference(etape)
+        histoires.append(etape[-1])
+        #print(etape)
+    somme += sum(histoires)
+print(somme)
