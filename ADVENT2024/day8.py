1987cb935456031243bb9c48d3bc12456f067abf
Habibou Halirou <habibhalirou@gmail.com>
2024-12-30 09:05:41 +0100

    day 8

--- ADVENT2024/day8.py
+++ ADVENT2024/day8.py
@@ -8,6 +8,26 @@ def get_coordonnees(puzzle):
                 coordonnes.setdefault(char, []).append((x, y))
     return coordonnes
 
+def calcul_antinoeud(coordonnees, longueur, hauteur):
+    antinoeuds = set()
+    for lettre, liste_tuples in coordonnees.items():
+
+        for i in range(len(liste_tuples)):
+            for j in range(i + 1, len(liste_tuples)):
+                # je calcul les coordinates des deux antinoeuds
+                antinoeud1X = liste_tuples[i][0] - (liste_tuples[j][0] - liste_tuples[i][0])
+                antinoeud1Y = liste_tuples[i][1] - (liste_tuples[j][1] - liste_tuples[i][1])
+                antinoeud2X = liste_tuples[j][0] + (liste_tuples[j][0] - liste_tuples[i][0])
+                antinoeud2Y = liste_tuples[j][1] + (liste_tuples[j][1] - liste_tuples[i][1])
+
+                if 0 <= antinoeud1X <= longueur and 0 <= antinoeud1Y <= hauteur:
+                    # .add() ajoute l'élement s'il n'existe pas déjà
+                    antinoeuds.add((antinoeud1X, antinoeud1Y))
+
+                if 0 <= antinoeud2X <= longueur and 0 <= antinoeud2Y <= hauteur:
+                    antinoeuds.add((antinoeud2X, antinoeud2Y))
+    return antinoeuds
+
 # Un peu basique 😅
 with open("entree", 'r') as file:
     puzzle = [line.strip() for line in file.readlines()]
@@ -16,22 +36,5 @@ longueur = len(puzzle[1])-1
 hauteur = len(puzzle)-1
 
 coordonnees = get_coordonnees(puzzle)
-antinoeuds = set()
-for lettre, liste_tuples in coordonnees.items():
-
-    for i in range(len(liste_tuples)):
-        for j in range(i + 1, len(liste_tuples)):
-            # je calcul les coordinates des deux antinoeuds
-            antinoeud1X = liste_tuples[i][0] - (liste_tuples[j][0] - liste_tuples[i][0])
-            antinoeud1Y = liste_tuples[i][1] - (liste_tuples[j][1] - liste_tuples[i][1])
-            antinoeud2X = liste_tuples[j][0] + (liste_tuples[j][0] - liste_tuples[i][0])
-            antinoeud2Y = liste_tuples[j][1] + (liste_tuples[j][1] - liste_tuples[i][1])
-
-            if 0 <= antinoeud1X <= longueur and 0 <= antinoeud1Y <= hauteur:
-                # .add() ajoute l'élement s'il n'existe pas déjà
-                antinoeuds.add((antinoeud1X, antinoeud1Y))
-
-            if 0 <= antinoeud2X <= longueur and 0 <= antinoeud2Y <= hauteur:
-                antinoeuds.add((antinoeud2X, antinoeud2Y))
-
+antinoeuds = calcul_antinoeud(coordonnees, longueur, hauteur)
 print(len(antinoeuds))
