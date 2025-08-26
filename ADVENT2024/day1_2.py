ca84453a8d99b9650d717d422e3b7bb4c794ed00
Hablna <145856383+Hablna@users.noreply.github.com>
2025-08-27 00:06:55 +0200

    day 1_2 achieved

--- ADVENT2024/day1_2.py
+++ ADVENT2024/day1_2.py
@@ -1,5 +1,12 @@
 with open("entree", "r") as f:
     colonne1, colonne2 = zip(*(map(int, line.split()) for line in f))
 
+similarite = 0
 for i in colonne1:
-    for j in colonne2
+    count = 0
+    for j in colonne2:
+        if i == j:
+            count += 1
+    similarite += i*count
+
+print(similarite)
