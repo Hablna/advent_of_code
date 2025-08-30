1b44eca0dd1e3abb97715e9410849f708a994325
Hablna <145856383+Hablna@users.noreply.github.com>
2025-08-31 00:50:03 +0200

    day 5 part2 Start

---
+++ ADVENT2024/day5_2.py
@@ -0,0 +1,19 @@
+with open("entree", "r") as f:
+    ordres, liste = f.read().strip().split("\n\n")
+    ordres = {tuple(ordre.split("|")) for ordre in ordres.split("\n")}
+    listes = [ligne.split(",") for ligne in liste.split("\n")]
+
+somme = 0
+for i in listes:
+    ok = True
+    for j in ordres:
+        if (j[0] in i) and (j[1] in i) and (i.index(j[0]) > i.index(j[1])):
+            ok = False
+            a = i.pop(i.index(j[0]))
+            i.insert(i.index(j[1])-1, a)
+
+    if not ok :
+        somme += int(i[len(i)//2])
+        print(i)
+
+print(somme)
