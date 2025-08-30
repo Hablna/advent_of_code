7ae91f8a743ee1300535629239dfa36c13dddd14
Hablna <145856383+Hablna@users.noreply.github.com>
2025-08-31 00:33:50 +0200

    day 5 part1

--- ADVENT2024/day5.py
+++ ADVENT2024/day5.py
@@ -1,11 +1,16 @@
-
 with open("entree", "r") as f:
     ordres, liste = f.read().strip().split("\n\n")
     ordres = {tuple(ordre.split("|")) for ordre in ordres.split("\n")}
     listes = [ligne.split(",") for ligne in liste.split("\n")]
 
-for i in ordres:
-    for j in listes:
-        if (i[0] and i[1]) in j :
-            print(f"{i[0]} et {i[1]}existent dans", j)
- # j'arrive
+somme = 0
+for i in listes:
+    ok = True
+    for j in ordres:
+        if (j[0] in i) and (j[1] in i) and (i.index(j[0]) > i.index(j[1])):
+            ok = False
+            break
+    if ok :
+        somme += int(i[len(i)//2])
+
+print(somme)
