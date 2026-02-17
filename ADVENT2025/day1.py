919ffac9c82a6ce1055da5bd147d8e9959943c4d
Hablna <145856383+Hablna@users.noreply.github.com>
2026-02-17 15:46:04 +0100

    Add day2025 class for password calculation

---
+++ ADVENT2025/day1.py
@@ -0,0 +1,58 @@
+"""package org.example;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class day2025 {
+    String filePath = "C:\\Users\\HALIROUNAMANOU-32255\\IdeaProjects\\test\\src\\main\\java\\org\\example\\entree";
+
+    public ArrayList<String> day2025() {
+        ArrayList<String> instructions = new ArrayList<>();
+        try(FileReader fl = new FileReader(filePath);
+            BufferedReader bf = new BufferedReader(fl)){
+            String line;
+
+            while ((line = bf.readLine()) != null){
+                instructions.add(line);
+            }
+        }catch(IOException e){
+            System.out.println("erreur lecture: "+ e.getMessage());
+        }
+        return instructions;
+    }
+
+    public int calculPassword() {
+        ArrayList instructions = day2025();
+        int i=0;
+        int password = 50;
+        for (Object instruction : instructions) {
+            Pattern pattern = Pattern.compile("([A-Z])(\\d+)");
+            Matcher matcher = pattern.matcher(instruction.toString());
+            if (matcher.find()){
+
+                if ("R".equals(matcher.group(1))){
+                    password = cycle(password, Integer.parseInt(matcher.group(2)));
+                }else{
+                    password = cycle(password, -Integer.parseInt(matcher.group(2)));
+                }
+                if (password == 0){
+                    i=i+1;
+                }
+            }
+
+        }
+        return i;
+    }
+
+    public int cycle(int actuel, int modif) {
+        int max = 100;
+        // On ajoute 'max' avant le dernier modulo pour gérer les soustractions
+        return (actuel + modif % max + max) % max;
+    }
+}
