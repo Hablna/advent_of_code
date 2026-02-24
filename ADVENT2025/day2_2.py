83e0ff29b292a232d3eb4818c385a5fb7b555902
Hablna <145856383+Hablna@users.noreply.github.com>
2026-02-24 14:22:37 +0100

    Add day2025 class to process ID pairs

---
+++ ADVENT2025/day2_2.py
@@ -0,0 +1,46 @@
+"""//package org.example;
+
+public class day2025 {
+    //String filePath = "C:\\Users\\HALIROUNAMANOU-32255\\IdeaProjects\\test\\src\\main\\java\\org\\example\\entree";
+    String filePath = "C:\\Users\\habib\\IdeaProjects\\First_project\\src\\entree";
+
+    public String[] day2025() {
+        String input = "655-1102,2949-4331,885300-1098691,1867-2844,20-43,4382100-4484893,781681037-781860439,647601-734894,2-16,180-238,195135887-195258082,47-64,4392-6414,6470-10044,345-600,5353503564-5353567532,124142-198665,1151882036-1151931750,6666551471-6666743820,207368-302426,5457772-5654349,72969293-73018196,71-109,46428150-46507525,15955-26536,65620-107801,1255-1813,427058-455196,333968-391876,482446-514820,45504-61820,36235767-36468253,23249929-23312800,5210718-5346163,648632326-648673051,116-173,752508-837824";
+        String[] parsedInput = input.split(",");
+        return parsedInput;
+    }
+
+    public Long calculSommeIdsInvalid(){
+        String[] Ids = day2025();
+        Long SommeIds = 0L;
+        for (String pairIdString : Ids) {
+            String[] pairId = pairIdString.split("-");
+            Long id1 = Long.parseLong(pairId[0]) ;
+            Long id2 = Long.parseLong(pairId[1]);
+
+            for (Long id=id1; id<=id2; id++) {
+                String idString = Long.toString(id);
+                int length = idString.length();
+
+                for (int i = 1; i <= length/2; i++) {
+                    if (length % i != 0) continue;
+
+                    String pattern = idString.substring(0, i);
+                    StringBuilder sb = new StringBuilder();
+
+                    int repeat = length/i;
+                    for (int j = 0; j < repeat; j++) {
+                        sb.append(pattern);
+                    }
+                    if (sb.toString().equals(idString)) {
+                        SommeIds += id;
+                        break;
+                    }
+
+                }
+            }
+        }
+
+        return SommeIds;
+    }
+}
